b1349b24b42cc43ccc8ef8236b47dacce39552c3
Mr0rang3 <mr.0r4ng3@proton.me>
2025-02-24 16:24:02 -0400

    Add user schema

---
+++ src/app/schemas/users.py
@@ -0,0 +1,17 @@
+from typing import Annotated
+
+from pydantic import BaseModel, Field
+
+
+class UserBase(BaseModel):
+    full_name: Annotated[str, Field(max_length=100, examples=["John Doe"])]
+    username: Annotated[str, Field(max_length=100, examples=["johndoe"])]
+
+
+class UserCreate(UserBase):
+    password: Annotated[str, Field(max_length=255)]
+
+
+class UserPublic(UserBase):
+    id: int
+    is_active: bool
